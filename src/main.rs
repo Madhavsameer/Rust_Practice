d2aa365b6c0ee710eb3e442c4e33c753d2e28ab8
madhavsameer97 <madhavsameer97@gmail.com>
2024-11-19 02:04:55 +0000

    changed

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ fn main() {
 }
 
 fn hellosameer(){
-    let  c=8;
-    c=9;
-    println!("c: {}",c);
+    let a=5;
+    let b=7;
+    let c=a+b;
+    println!(" first number is {} second number is {} Sum of these number is {}",a,b,c);
     println!("Hello sameer");
 }
+
+
+
+
